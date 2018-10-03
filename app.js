aff6725ade44b5e5205d77e5b060f58335db60b8
Dinesh Patel <easymail3455@yahoo.com>
2018-10-03 20:16:10 +0000

    modified:app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+//to use git initialize git in the specific directory with 'git init'
+// without "git add app.js" app.js will show up as untracked files as git status
+//use 'git add app.js' to track app.js
+//after 'git add app.js' 'git status' will show new file:app.js under "changes to be commited"
+//git commit -m"place messsage here..all added file will be commited
+console.log("app.js");
