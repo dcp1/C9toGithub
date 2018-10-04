7c492cbb6cfad2509d2a1818543e21bae18f20e0
Dinesh Patel <easymail3455@yahoo.com>
2018-10-04 02:24:00 +0000

    commit app.js

--- app.js
+++ app.js
@@ -3,5 +3,7 @@
 //use 'git add app.js' to track app.js
 //after 'git add app.js' 'git status' will show new file:app.js under "changes to be commited"
 //git commit -m"place messsage here..all added file will be commited
+//add all tracked files one by one or use dot like 'git add .'to add all files ready for commit
+// only added files with "git add xxxx" will be commited with "git commit -m"xxx".
 console.log("app.js");
 console.log("new change");
